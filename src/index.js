8162dfe0a92960acfbe217aa8b3aac38fe08bcbd
Gabriel BF <gbbolonhez@gmail.com>
2025-06-03 23:51:44 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import * from "./services/cart.js";
+
+const cart = [];
---
+++ src/services/cart.js
@@ -0,0 +1,34 @@
+// Actions that the cart can perform:
+// Use cases:
+
+// -> Add item
+
+async function addItem(userCart) {
+
+}
+// -> Delete item
+
+async function deleteItem(userCart, name) {
+
+}
+
+
+// -> Remove item
+
+async function removeItem(userCart, index) {
+
+} 
+
+// -> Calculate total of the cart
+
+async function calcTotal(userCart) {
+
+}
+
+export default {
+    addItem,
+    deleteItem,
+    removeItem,
+    calcTotal
+}
+
