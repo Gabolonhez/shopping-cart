0f752247971f108037a2a4aafb037d3b578f1cb3
Gabriel BF <gbbolonhez@gmail.com>
2025-06-04 23:40:21 -0300

    feat: adding cart and item functions logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import * from "./services/cart.js";
+import cartService from "./services/cart.js";
+import { createItem } from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
+
+ 
+
+const item1 = await createItem("Hotwheels Ferrari", 20.99, 2);
+const item2 = await createItem("Hotwheels Lamborghini", 24.99, 2);  
+
+await cartService.addItem(myCart, item1);
+
+await cartService.addItem(myCart, item2);
+
+// await cartService.deleteItem(myCart, item2.name);
+// await cartService.deleteItem(myCart, item1.name);
+
+
+await cartService.calcTotal(myCart);
+ 
+cartService.displayCart(myCart); 
