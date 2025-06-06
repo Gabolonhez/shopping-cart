2a36fa01088c635dbcac4db818afe38513cf3371
Gabriel BF <gbbolonhez@gmail.com>
2025-06-05 23:06:45 -0300

    feat: adding delete and remove item functions

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ const item1 = await createItem("Hotwheels Ferrari", 20.99, 2);
 const item2 = await createItem("Hotwheels Lamborghini", 24.99, 2);  
 
 await cartService.addItem(myCart, item1);
-
 await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, item1);
+await cartService.removeItem(myCart, item2);
 
 // await cartService.deleteItem(myCart, item2.name);
 // await cartService.deleteItem(myCart, item1.name);
 
+await cartService.calcTotal(myCart);  
 
-await cartService.calcTotal(myCart);
- 
 cartService.displayCart(myCart); 
+
+
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,7 +1,6 @@
 // USE CASES:
 
 // -> Add item
-
 async function addItem(userCart, item) {
     userCart.push(item);
 }
@@ -15,25 +14,56 @@ async function calcTotal(userCart) {
 }
 
 // -> Delete item
-
 async function deleteItem(userCart, name) {
     const index = userCart.findIndex((item) => item.name === name);
 
-    if (index !== 1) {
+    if (index !== -1) {
         userCart.splice(index, 1);
     }
 }
 
 
-// -> Remove item
+// -> Remove item 
+async function removeItem(userCart, item) {
+    const indexFound = userCart.findIndex((p) => p.name === item.name) 
+   
+    // Found the item index
 
-async function removeItem(userCart, index) {
+    if (indexFound == -1) {
+        console.log("Item not found");
+        return;
+    }
 
-} 
+    // If Item > 1, then subtract 1 item
+
+    if (userCart[indexFound].quantity > 1) {
+        userCart[indexFound].quantity -= 1;
+        return;
+
+    }
+
+    // If Item = 1, then delet the item
+    if(userCart[indexFound].quantity = 1) {
+        userCart.splice(indexFound, 1);
+        return;
+    } 
 
 
-// Check cart
+}
 
+
+// -> Remove item by index
+// async function removeItem(userCart, index) {
+
+//     const deleteIndex = index - 1;
+
+//     if (index >= 1 && index <= userCart.length) {
+//         userCart.splice(deleteIndex, 1);
+//     }
+// } 
+
+
+// -> Check cart
 async function displayCart (userCart) {
     console.log("\n🛒 Cart list:")
     userCart.forEach((item, index) => {
